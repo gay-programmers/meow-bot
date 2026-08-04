1f15bdeb202c65d39391cf36968682b8d74f8db1
pludunn <dunn_isaac@icloud.com>
2026-08-04 00:28:30 -0400

    Added sync to bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,8 @@ bot = commands.Bot(command_prefix=prefix, intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
-    print(f"\033[1A\033[2K\rLogged in as {bot.user}")
+    print(f"\033[1A\033[2K\033[1A\033[2K\033[1A\033[2K\rLogged in as {bot.user}")
+    bot.tree.sync()
 
 
 def command(
