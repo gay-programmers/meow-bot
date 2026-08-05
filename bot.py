3ab89f7cd923828f5f350a9a5f8665cc7f7ec675
pludunn <dunn_isaac@icloud.com>
2026-08-04 22:40:07 -0400

    hi

--- bot.py
+++ bot.py
@@ -139,7 +139,7 @@ async def unban(ctx: commands.Context, user: discord.User) -> None:
     await ctx.guild.unban(user)
 
 
-@command(["manage_server"])
+@command(["manage_guild"])
 async def kickbot(ctx: commands.Context) -> None:
     if ctx.guild is None:
         raise ValueError("This command can only be used in a server.")
